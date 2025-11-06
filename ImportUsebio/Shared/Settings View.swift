ca607cf3987f25707bb2905fec0585f31c3b152b
Marc Shearer <marc@sheareronline.com>
2025-11-06 13:57:33 +0000

    Add help and make descriptions clearer in Settings

--- ImportUsebio/Shared/Settings View.swift
+++ ImportUsebio/Shared/Settings View.swift
@@ -34,8 +34,10 @@ struct SettingsView: View {
                                 HStack {
                                     
                                     InputInt(title: "SBU Number:", field:$settings.maxNationalIdNumber, topSpace: 0, leadingSpace: 30, width: 80, inlineTitle: true, inlineTitleWidth: 100)
+                                        .help("SBU Numbers above this value will be highlighted")
                                     
                                     InputFloat(title: "Points award:", field: $settings.maxPoints, topSpace: 0, leadingSpace: 30, width: 50, inlineTitle: true, inlineTitleWidth: 100)
+                                        .help("Point awards to a player above this will be highlighted")
                                     
                                     Spacer()
                                 }
@@ -47,8 +49,10 @@ struct SettingsView: View {
                                 HStack {
                                     
                                    Input(title: "Status  :", field: $settings.goodStatus, topSpace: 0, leadingSpace: 30, width: 80, inlineTitle: true, inlineTitleWidth: 100, isEnabled: true)
+                                        .help("Value of status column in download that is considered 'Active'")
                                     
                                     Input(title: "Payment  :", field: $settings.goodPaymentStatus, topSpace: 0, leadingSpace: 30, width: 250, inlineTitle: true, inlineTitleWidth: 120, isEnabled: true)
+                                        .help("Value in current season payment status column that is considered 'Paid'")
                                     
                                     Spacer()
                                 }
@@ -59,8 +63,10 @@ struct SettingsView: View {
                                     Spacer().frame(width: 220)
                                     
                                     InputInt(title: "Ignore months :", field: $settings.ignorePaymentFrom, topSpace: 0, leadingSpace: 30, width: 40, inlineTitle: true, inlineTitleWidth: 114, isEnabled: true)
+                                        .help("Months after this month and before the 'to' month will be ignored for payment warning")
                                     
                                     InputInt(title: "to :", field: $settings.ignorePaymentTo, topSpace: 0, leadingSpace: 30, width: 40, inlineTitle: true, inlineTitleWidth: 30, isEnabled: true)
+                                        .help("Months up to this month after the 'from' month will be ignored for payment warning")
                                     
                                     Spacer()
                                 }
@@ -71,9 +77,11 @@ struct SettingsView: View {
                                 Spacer().frame(height: 8)
                                 HStack {
                                     
-                                    InputInt(title: "Field entry:", field: $settings.largestFieldSize, topSpace: 0, leadingSpace: 30, width: 80, inlineTitle: true, inlineTitleWidth: 100)
+                                    InputInt(title: "Max round entry:", field: $settings.largestFieldSize, topSpace: 0, leadingSpace: 30, width: 80, inlineTitle: true, inlineTitleWidth: 150)
+                                        .help("Maximum number of participants (pairs/teams) in a round")
                                     
-                                    InputInt(title: "Players:", field: $settings.largestPlayerCount, topSpace: 0, leadingSpace: 30, width: 80, inlineTitle: true, inlineTitleWidth: 100)
+                                    InputInt(title: "Max round players:", field: $settings.largestPlayerCount, topSpace: 0, leadingSpace: 30, width: 80, inlineTitle: true, inlineTitleWidth: 150)
+                                        .help("Maximum number of players in a round")
                                     
                                     Spacer()
                                 }
@@ -84,9 +92,11 @@ struct SettingsView: View {
                                 Spacer().frame(height: 8)
                                 HStack {
                                     
-                                    InputFloat(title: "Default zoom:", field: $settings.defaultWorksheetZoom, topSpace: 0, leadingSpace: 30, width: 80, inlineTitle: true, inlineTitleWidth: 100)
+                                    InputFloat(title: "Default zoom:", field: $settings.defaultWorksheetZoom, topSpace: 0, leadingSpace: 30, width: 80, inlineTitle: true, inlineTitleWidth: 150)
+                                        .help("Default zoom factor of spreadsheet")
                                     
-                                    InputInt(title: "Lines/page:", field: $settings.linesPerFormattedPage, topSpace: 0, leadingSpace: 30, width: 80, inlineTitle: true, inlineTitleWidth: 100)
+                                    InputInt(title: "Lines/page:", field: $settings.linesPerFormattedPage, topSpace: 0, leadingSpace: 30, width: 80, inlineTitle: true, inlineTitleWidth: 150)
+                                        .help("Default lines/page on the formatted tab of the spreadsheet")
                                     
                                     Spacer()
                                 }
@@ -97,15 +107,18 @@ struct SettingsView: View {
                                 
                                 HStack {
                                     Input(title: "Filename:", field: $settings.userDownloadData, leadingSpace: 30, width: 160, inlineTitle: true, inlineTitleWidth: 100, isEnabled: true)
+                                        .help("Filename of the downloaded user data (including extension)")
                                     
                                     InputInt(title: "First Row:", field: $settings.userDownloadMinRow, topSpace: 0, leadingSpace: 30, width: 80, inlineTitle: true, inlineTitleWidth: 80)
-                                    
-                                    InputInt(title: "Last Row:", field: $settings.userDownloadMaxRow, topSpace: 0, leadingSpace: 30, width: 80, inlineTitle: true, inlineTitleWidth: 80)
+                                        .help("First data row in the downloaded user data")
+                                    InputInt(title: "Max Row:", field: $settings.userDownloadMaxRow, topSpace: 0, leadingSpace: 30, width: 80, inlineTitle: true, inlineTitleWidth: 80)
+                                        .help("Maximum data row in the downloaded user data")
                                     
                                     Spacer()
                                 }
                                 HStack {
                                     Input(title: "Frozen file:", field: $settings.userDownloadFrozenData, leadingSpace: 30, width: 160, inlineTitle: true, inlineTitleWidth: 100, isEnabled: true)
+                                        .help("Filename of the downloaded frozen user data (including extension)")
                                     Spacer()
                                 }
                             }
@@ -119,12 +132,16 @@ struct SettingsView: View {
                                     HStack {
                                         
                                         Input(title: "National ID:", field: $settings.userDownloadNationalIdColumn, topSpace: 0, leadingSpace: 30, width: 40, inlineTitle: true, inlineTitleWidth: 100, isEnabled: true)
+                                            .help("Column letter(s) of the national ID column in the downloaded data")
                                         
                                         Input(title: "First Name:", field: $settings.userDownloadFirstNameColumn, topSpace: 0, leadingSpace: 12, width: 40, inlineTitle: true, inlineTitleWidth: 100, isEnabled: true)
+                                            .help("Column letter(s) of the first name column in the downloaded data")
                                         
                                         Input(title: "Other Names:", field: $settings.userDownloadOtherNamesColumn, topSpace: 0, leadingSpace: 12, width: 40, inlineTitle: true, inlineTitleWidth: 100, isEnabled: true)
+                                            .help("Column letter(s) of the other names column in the downloaded data")
                                         
                                         Input(title: "Rank:", field: $settings.userDownloadRankColumn, topSpace: 0, leadingSpace: 12, width: 40, inlineTitle: true, inlineTitleWidth: 100, isEnabled: true)
+                                            .help("Column letter(s) of the rank code column in the downloaded data")
                                         
                                         Spacer()
                                     }
@@ -132,12 +149,16 @@ struct SettingsView: View {
                                     HStack {
                                         
                                         Input(title: "Email:", field: $settings.userDownloadEmailColumn, topSpace: 0, leadingSpace: 30, width: 40, inlineTitle: true, inlineTitleWidth: 100, isEnabled: true)
+                                            .help("Column letter(s) of the email column in the downloaded data")
                                         
                                         Input(title: "Home Club:", field: $settings.userDownloadHomeClubColumn, topSpace: 0, leadingSpace: 12, width: 40, inlineTitle: true, inlineTitleWidth: 100, isEnabled: true)
+                                            .help("Column letter(s) of the home club column in the downloaded data")
                                         
                                         Input(title: "Status:", field: $settings.userDownloadStatusColumn, topSpace: 0, leadingSpace: 12, width: 40, inlineTitle: true, inlineTitleWidth: 100, isEnabled: true)
+                                            .help("Column letter(s) of the status column in the downloaded data")
                                         
                                         Input(title: "Payment:", field: $settings.userDownloadPaymentStatusColumn, topSpace: 0, leadingSpace: 12, width: 40, inlineTitle: true, inlineTitleWidth: 100, isEnabled: true)
+                                            .help("Column letter(s) of the current season payment status column in the downloaded data")
                                         
                                         Spacer()
                                         
@@ -145,7 +166,8 @@ struct SettingsView: View {
                                     
                                     HStack {
                                         
-                                        Input(title: "Other Union:", field: $settings.userDownloadOtherUnionColumn, topSpace: 0, leadingSpace: 30, width: 40, inlineTitle: true, inlineTitleWidth: 100, isEnabled: true)
+                                        Input(title: "Union (NBO):", field: $settings.userDownloadOtherUnionColumn, topSpace: 0, leadingSpace: 30, width: 40, inlineTitle: true, inlineTitleWidth: 100, isEnabled: true)
+                                            .help("Column letter(s) of the union (NBO) column in the downloaded data")
                                         
                                         Spacer()
                                     }
